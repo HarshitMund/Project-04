a014c0f1a9d09aebdd9ff041452009111a322c00
HarshitMund <harshitmund6@gmail.com>
2026-04-25 15:08:27 +0530

    Added course usecase

---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/test/TestCourseModel.java
@@ -0,0 +1,117 @@
+package in.co.rays.proj4.test;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import in.co.rays.proj4.bean.CourseBean;
+import in.co.rays.proj4.exception.ApplicationException;
+import in.co.rays.proj4.model.CourseModel;
+
+public class TestCourseModel {
+
+	public static CourseModel model = new CourseModel();
+
+	public static void main(String[] args) throws ApplicationException {
+
+//		testAdd();
+//		testUpdate();
+//		testDelete();
+//		testFindByPk();
+//		testFindByName();
+		testSearch();
+
+	}
+
+	public static void testAdd() throws ApplicationException {
+
+		CourseBean bean = new CourseBean();
+
+		bean.setName("BTech");
+		bean.setDescription("Coumpter");
+		bean.setDuration("4 Year");
+		bean.setCreatedBy("admin");
+		bean.setModifiedBy("admin");
+		bean.setCreatedDatatime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		long id = model.add(bean);
+		System.out.println("Record inserted at ID: " + id);
+	}
+
+	public static void testUpdate() throws ApplicationException {
+
+		CourseBean bean = new CourseBean();
+
+		bean.setId(3);
+		bean.setName("BTech");
+		bean.setDescription("Mechanical");
+		bean.setDuration("4 Year");
+		bean.setCreatedBy("System");
+		bean.setModifiedBy("System");
+		bean.setCreatedDatatime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		model.update(bean);
+	}
+
+	public static void testDelete() throws ApplicationException {
+
+		CourseBean bean = new CourseBean();
+
+		bean.setId(3);
+
+		model.delete(bean);
+	}
+
+	public static void testFindByPk() throws ApplicationException {
+
+		CourseBean bean = model.findByPk(1);
+
+		System.out.println(bean.getId());
+		System.out.println(bean.getName());
+		System.out.println(bean.getDuration());
+		System.out.println(bean.getDescription());
+		System.out.println(bean.getCreatedBy());
+		System.out.println(bean.getModifiedBy());
+		System.out.println(bean.getCreatedDatatime());
+		System.out.println(bean.getCreatedDatatime());
+	}
+
+	public static void testFindByName() throws ApplicationException {
+
+		CourseBean bean = model.findByName("BTech");
+
+		System.out.println(bean.getId());
+		System.out.println(bean.getName());
+		System.out.println(bean.getDuration());
+		System.out.println(bean.getDescription());
+		System.out.println(bean.getCreatedBy());
+		System.out.println(bean.getModifiedBy());
+		System.out.println(bean.getCreatedDatatime());
+		System.out.println(bean.getCreatedDatatime());
+	}
+
+	public static void testSearch() throws ApplicationException {
+
+		CourseBean bean = new CourseBean();
+		List<CourseBean> list = new ArrayList<CourseBean>();
+
+		list = model.search(bean, 1, 10);
+		Iterator<CourseBean> it = list.iterator();
+
+		while (it.hasNext()) {
+			bean = it.next();
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getName());
+			System.out.print("\t" + bean.getDuration());
+			System.out.print("\t" + bean.getDescription());
+			System.out.print("\t" + bean.getCreatedBy());
+			System.out.print("\t" + bean.getModifiedBy());
+			System.out.print("\t" + bean.getCreatedDatatime());
+			System.out.println("\t" + bean.getCreatedDatatime());
+		}
+	}
+}
